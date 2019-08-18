8f4859499d2e16e5fcd604027d57621ede2b1048
Jaeyoung Lee <jaeyounglee@Jaeyoungui-iMac.local>
2019-08-19 01:45:59 +0900

    Modified checkOut() method error - don't splice array idx over 1.

--- front-end-beginer/assignments/week_02/hotel-app.js
+++ front-end-beginer/assignments/week_02/hotel-app.js
@@ -24,16 +24,17 @@ const Hotel = {
     },
     checkOut: function (name) {
         //인원, 잘못된 값, 빈값에 대한 검증
-        let msg = (this.customers.length===0 && name + '님은 체크인 명단에 없습니다. (체크인 인원이 없음.)')
-                || (name !== '' && typeof name === 'string' && name) 
+        let msg = (name !== '' && typeof name === 'string' && name) 
                 || (name === '' && '고객님의 성함이 입력되지 않았습니다.')
                 || (typeof name !== 'String' && name + '은 문자열이 아닙니다.')
                 || '알 수 없는 오류입니다. 관리자에게 문의하세요.';
         //체크아웃 및 없는인원 검증
         for (let i=0; i<this.customers.length && msg===name; i++) {
             msg = (this.customers[i]===name && this.customers.splice(i, 1)[0] + '님 감사합니다. 안녕히 가세요.')
-                || name + '님은 체크인 명단에 없습니다.';
+                || msg;
         }
+        msg = (msg===name && name + '님은 체크인 명단에 없습니다.') || msg;
+
         //return
         return msg;
     }
@@ -44,15 +45,15 @@ console.log(Hotel.checkIn());
 console.log(Hotel.checkIn(12345));
 console.log(Hotel.checkIn(''));
 console.log(Hotel.checkIn('jylee'));
-console.log(Hotel.checkIn('jylee'));
+console.log(Hotel.checkIn('이재영'));
 console.log(Hotel.checkIn('ㅇㅈㅇ'));
 console.log(Hotel.getStatus());
 console.log(Hotel.checkOut());
 console.log(Hotel.checkOut(12345));
-console.log(Hotel.checkOut('jylee'));
+console.log(Hotel.checkOut('ㅇㅈㅇ'));
 console.log(Hotel.checkOut('jylee'));
 console.log(Hotel.checkOut(''));
 console.log(Hotel.getStatus());
-console.log(Hotel.checkOut('ㅇㅈㅇ'));
+console.log(Hotel.checkOut('이재영'));
 console.log(Hotel.checkOut('ㅇㅈㅇ'));
 console.log(Hotel.getStatus());
