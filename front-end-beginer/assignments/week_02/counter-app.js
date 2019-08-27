703b48cd6b474424f4614ee4770f925503b52cab
Jaeyoung Lee <jaeyounglee@Jaeyoungui-iMac.local>
2019-08-27 20:43:47 +0900

    Add a method and commit

--- front-end-beginer/assignments/week_02/counter-app.js
+++ front-end-beginer/assignments/week_02/counter-app.js
@@ -4,24 +4,25 @@ const Counter = {
         return this.count;
     },
     increment : function(num) {
-        const int = num || 1;
-        this.count += int;
+        this.calculate(num || 1);
     },
     decrement : function(num) {
-        const int = num || 1;
-        this.count -= int;
-    }
+        this.calculate((num && -1*num) || -1);
+    },
+    calculate : function(num) {
+        this.count += num;
+    },
 }
-/*
 console.log(Counter.getCount()); // 0
 Counter.increment();
 Counter.increment();
 console.log(Counter.getCount()); // 2
 Counter.decrement();
 console.log(Counter.getCount()); // 1
-*/
+/*
 console.log(Counter.getCount()); // 0
 Counter.increment(5);
 console.log(Counter.getCount()); // 5
 Counter.decrement(2);
 console.log(Counter.getCount()); // 3
+*/
